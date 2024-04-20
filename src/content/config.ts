b8b59f1af5105ce1f74f934df42045891a76c65c
Daniel Milenkovic <milendan@init7.net>
2024-04-20 17:29:25 +0200

    make gallery work and finalize basic design

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { z, defineCollection } from "astro:content";
+
+const galleryCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+    hidden: z.boolean().optional(),
+  }),
+});
+
+export const collections = {
+  gallery: galleryCollection,
+};
